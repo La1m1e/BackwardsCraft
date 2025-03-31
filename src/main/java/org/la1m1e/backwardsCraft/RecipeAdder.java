e3c82a7718e23767c7c06d52cd872fa2005c3088
La1m1e <lysenko.sunny@gmail.com>
2025-03-31 21:47:15 +0300

    balls

--- src/main/java/org/la1m1e/backwardsCraft/RecipeAdder.java
+++ src/main/java/org/la1m1e/backwardsCraft/RecipeAdder.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapelessRecipe;
 
 public class RecipeAdder {
@@ -15,7 +16,7 @@ public class RecipeAdder {
         Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftWool"), new ItemStack(Material.STRING,1)).addIngredient(ItemStack.of(Material.WHITE_WOOL)));
         Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftPlanks"), new ItemStack(Material.OAK_LOG,1)).addIngredient(ItemStack.of(Material.OAK_PLANKS,4)));
         Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftFlintSt"), new ItemStack(Material.IRON_INGOT,1)).addIngredient(ItemStack.of(Material.FLINT_AND_STEEL,1)));
-        Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftPickaxe"), new ItemStack(Material.DIAMOND,3)).addIngredient(ItemStack.of(Material.DIAMOND_PICKAXE)));
+        Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftPickaxe"), new ItemStack(Material.DIAMOND,3)).addIngredient(new RecipeChoice.MaterialChoice(Material.DIAMOND_PICKAXE)));
         Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftAxe"), new ItemStack(Material.IRON_INGOT,3)).addIngredient(ItemStack.of(Material.IRON_AXE)));
         Bukkit.addRecipe(new ShapelessRecipe(new NamespacedKey(plugin, "UncraftBucket"), new ItemStack(Material.IRON_INGOT,3)).addIngredient(ItemStack.of(Material.BUCKET)));
     }
